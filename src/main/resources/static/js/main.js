71a48c7a45dccd57e30acad5112ec12ba371e8be
Sohesh TS <soheshts@gmail.com>
2025-08-18 16:15:35 +0530

    Updated UI

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -82,14 +82,13 @@ function onMessageReceived(payload) {
         if (message.sender === "BOT") {
             messageElement.classList.add('chat-message');
             var avatarElement = document.createElement('i');
-            var avatarText = document.createTextNode(message.sender[0]);
+            var avatarText = document.createTextNode("M");
             avatarElement.appendChild(avatarText);
             avatarElement.style['background-color'] = getAvatarColor(message.sender);
-
             messageElement.appendChild(avatarElement);
 
             var usernameElement = document.createElement('span');
-            var usernameText = document.createTextNode(message.sender);
+            var usernameText = document.createTextNode("Mr Ferguson");
             usernameElement.appendChild(usernameText);
             messageElement.appendChild(usernameElement);
         } else {
